fbf874231b17742cdc8421338daf309a5594e41a
Max2411 <maxvankemenade@gmail.com>
2020-02-19 20:25:21 +0100

    answer extra

--- MasterMind/algoritme3_0.py
+++ MasterMind/algoritme3_0.py
@@ -7,3 +7,29 @@ def algoritme3():
     correctAns = answerInput()
     print(guess)
     while True:
+        tries += 1
+        newLst = []
+        if compair(guess, correctAns, tries) == True:
+            break
+        else:
+            black, white = feedback(guess, correctAns)  # gives feedback for the guess compaired to the answer
+            print(black, " black")
+            print(white, "white")
+
+            for item in possibilitie:
+
+                blackP, whiteP = feedback(item,
+                                          guess)  # deletes al items with the same feedback compaired to the guess of the computerer
+
+                if black != blackP or white != whiteP:
+                    newLst.append(item)  # First item removed the item but that made the item from possiblite jump up
+                    # so it skipped some items.
+        for item in newLst:  # this for loop is to prevent that
+
+            delItem = item
+            if delItem in possibilitie:
+                possibilitie.remove(delItem)
+            for i in possibilitie:
+                for j in possibilitie
+                    black1, white1 = feedback(i,j)
+                    if
--- MasterMind/correctAnswer.py
+++ MasterMind/correctAnswer.py
@@ -2,10 +2,18 @@ def answerInput():
     print("Enter the code you want the computer to guess on the next line.")
     answer = input("Choose 4 colours for your code((R)ed, (B)lue, (G)reen, (Y)ellow, (P)ink, (W)hite): ")
     answer = answer.upper()
-    answerTuple = (answer[0], answer[1], answer[2], answer[3])
+
+    if len(answer) != 4:
+        print("Not the right lengt")
+        return answerInput()
+    else:
+        answerTuple = (answer[0], answer[1], answer[2], answer[3])
     print(answerTuple)
 
+
+
     return answerTuple
+answerInput()
 
 def compair(guess,correctAns,tries):
     if guess == correctAns:
